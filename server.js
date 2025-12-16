742ec57cac3f8802b4b3effb80a4b4bdb11f15d1
rockhocduong2-design <rockhocduong2@gmail.com>
2025-12-16 22:10:33 +0700

    commit2

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
     // Chỗ này để bạn sửa nội dung khi thầy yêu cầu test CI/CD
-    res.send('<h1>Dự án Backend - Đã kết nối Database thành công!</h1>');
+    res.send('<h1>Dự án Backend - Đã kết nối Database thành công! HIHI</h1>');
 });
 
 app.listen(PORT, () => {
