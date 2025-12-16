e8f1e969519d8487fe9383193d6f2517bf8c8f7a
rockhocduong2-design <rockhocduong2@gmail.com>
2025-12-16 23:18:29 +0700

    Ket noi Database that

--- server.js
+++ server.js
@@ -1,12 +1,25 @@
 const express = require('express');
+const { Pool } = require('pg'); // Cần chạy lệnh: npm install pg
 const app = express();
-const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-    // Chỗ này để bạn sửa nội dung khi thầy yêu cầu test CI/CD
-    res.send('<h1>Dự án Backend - Đã kết nối Database thành công! HIHI</h1>');
+// Dán cái link External Database URL bạn vừa copy vào đây
+const connectionString = 'postgresql://my_database_cay4_user:t2chs6B1vmZqGYgUQx3ZjAxupMBpaiwk@dpg-d50oarggjchc73chchb0-a.oregon-postgres.render.com/my_database_cay4';
+
+const pool = new Pool({
+  connectionString: connectionString,
 });
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+app.get('/', async (req, res) => {
+  try {
+    const result = await pool.query('SELECT NOW()'); // Lệnh test kết nối
+    res.json({
+      message: "Kết nối Database thành công!",
+      db_time: result.rows[0].now,
+      project: "Backend Project 2 - MSSV của bạn"
+    });
+  } catch (err) {
+    res.status(500).send("Lỗi kết nối Database: " + err.message);
+  }
 });
+
+app.listen(3000, () => console.log('Server is running!'));
